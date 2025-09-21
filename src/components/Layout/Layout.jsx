6454e916b71e9a033752cff8d13d1a91fa139633
adityashetty35 <adityashetty35@gmail.com>
2025-09-21 11:13:48 +0530

    feat(layout): replace resume button text with SVG icon

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -118,7 +118,17 @@ function Layout() {
                 rel="noreferrer"
                 className="custom-btn resume-btn"
               >
-                &#9654; Resume
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  width="20"
+                  height="20"
+                  aria-hidden="true"
+                  className="icon"
+                >
+                  <path d="M5 2.69127C5 1.93067 5.81547 1.44851 6.48192 1.81506L23.4069 11.1238C24.0977 11.5037 24.0977 12.4963 23.4069 12.8762L6.48192 22.1849C5.81546 22.5515 5 22.0693 5 21.3087V2.69127Z" />
+                </svg>
+                Resume
               </a>
 
               <a
